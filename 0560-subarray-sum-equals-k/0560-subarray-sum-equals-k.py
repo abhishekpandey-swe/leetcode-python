bb5170f50d855e00f37ee59fb6a9f57edf7b6492
abhishek-ai <abhishekcs.it06@gmail.com>
2026-07-29 00:37:52 +0530

    Time: 24 ms (95.83%), Space: 21.8 MB (84.64%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def subarraySum(self, nums, k):
         """
-        Time Complexity : O(n)
-        Space Complexity : O(1)
-        
         :type nums: List[int]
         :type k: int
         :rtype: int
